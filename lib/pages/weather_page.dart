b3dbbb2b063f02b4ef8f8531aaa3da4fecc5d189
Jonathan Setiawan <jonathanrustam2@gmail.com>
2025-04-07 00:57:27 +0900

    fix

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
